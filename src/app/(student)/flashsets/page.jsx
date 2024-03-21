fda27aa2c94d56dee951210366e2e882b8ca87bc
nhontx <kevintran351996@gmail.com>
2024-03-21 10:12:53 +0700

    view all

--- src/app/(student)/flashsets/page.jsx
+++ src/app/(student)/flashsets/page.jsx
@@ -9,103 +9,88 @@ import React, { useRef, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Page() {
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(20);
   const total = useRef(0);
-  //   const flashsets = useQuery({
-  //     queryKey: ['flashsets', page, 10],
-  //     queryFn: () =>
-  //       flashsetsAPI.getFlashsets({ page, perPage: 10 }).then((res) => {
-  //         return res.data
-  //       }),
-  //   })
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const {
-    status,
-    data,
-    error,
-    isFetching,
-    isFetchingNextPage,
-    isFetchingPreviousPage,
-    fetchNextPage,
-    fetchPreviousPage,
-    hasNextPage,
-    hasPreviousPage,
-  } = useInfiniteQuery(
-    ["projects"],
-    async ({ pageParam = 1 }) => {
-      const res = await flashsetsAPINew.getFlashsets({
-        page: pageParam,
-        perPage: 20,
-      });
-      total.current = res?.data.total;
+  const fecthData = async () => {
+    const res = await flashsetsAPINew.getFlashsets({
+      page: 1,
+      perPage: page,
+    });
+    total.current = res?.data.total;
+    setData(res?.data?.data);
+  };
 
-      return res;
-    },
-    {
-      //   getPreviousPageParam: (firstPage) => firstPage.previousId ?? undefined,
-      getNextPageParam: (lastPage) => {
-        // const previousPage = lastPage.info.prev
-        //   ? +lastPage.info.prev.split('=')[1]
-        //   : 0
-        const currentPage = lastPage.data.page;
-        if (lastPage.data.page * 20 >= lastPage.data.total) return false;
-        // if (currentPage === lastPage.info.pages) return false
-        return currentPage + 1;
-      },
+  React.useEffect(() => {
+    const fetchLoading = async () => {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500);
+    };
+    fetchLoading();
+  }, []);
+
+  React.useEffect(() => {
+    fecthData();
+  }, [page]);
+
+  React.useEffect(() => {
+    function handleScroll() {
+      const { scrollTop, clientHeight, scrollHeight } =
+        document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight - 10) {
+        if (page <= Number(total.current)) {
+          setPage((prevPage) => prevPage + 10);
+        }
+      }
     }
-  );
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [page, total.current]);
+  
   const loadMore = () => {
     if (flashsets.isLoading) return;
     setPage((s) => s + 1);
   };
 
-  if (status === "loading")
-    return <Spinner size="lg" className="mt-8 self-center" />;
-  if (status === "error")
-    return <Spinner size="lg" color="danger" className="mt-8 self-center" />;
+  if (loading) return <Spinner size="lg" className="mt-8 self-center" />;
+  // if (status === "error")
+  //   return <Spinner size="lg" color="danger" className="mt-8 self-center" />;
 
   return (
     <div className="flex flex-col py-4">
       <h2 className="text-xl">Flash Sets</h2>
-      <InfiniteScroll
-        dataLength={total.current}
-        next={() => {
-          fetchNextPage();
-        }}
-        hasMore={!!hasNextPage}
-        loader={<h4>Loading...</h4>}
+
+      <div
+        className={`grid grid-cols-2 gap-4 py-4 md:grid-cols-3 lg:grid-cols-5`}
       >
-        <div
-          className={`grid grid-cols-2 gap-4 py-4 md:grid-cols-3 lg:grid-cols-5`}
-        >
-          {data?.pages?.map((page) => (
-            <React.Fragment key={page.nextId}>
-              {page?.data?.data?.map((i) => (
-                <Link
-                  key={i.id}
-                  href={`/flashsets/${i.id}`}
-                  className="cursor-pointer"
-                >
-                  <Card shadow="sm" className="w-full">
-                    <CardBody className="overflow-visible p-0">
-                      <Image
-                        width={320}
-                        height={320}
-                        alt={i.name}
-                        className="aspect-square w-full"
-                        src={i.image}
-                      />
-                    </CardBody>
-                    <CardFooter className="justify-between text-small">
-                      <b className="line-clamp-1">{i.name}</b>
-                    </CardFooter>
-                  </Card>
-                </Link>
-              ))}
-            </React.Fragment>
-          ))}
-        </div>
-      </InfiniteScroll>
+        {data?.map((i) => (
+          <Link
+            key={i.id}
+            href={`/flashsets/${i.id}`}
+            className="cursor-pointer"
+          >
+            <Card shadow="sm" className="w-full">
+              <CardBody className="overflow-visible p-0">
+                <Image
+                  width={320}
+                  height={320}
+                  alt={i.name}
+                  className="aspect-square w-full"
+                  src={i.image}
+                />
+              </CardBody>
+              <CardFooter className="justify-between text-small">
+                <b className="line-clamp-1">{i.name}</b>
+              </CardFooter>
+            </Card>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }

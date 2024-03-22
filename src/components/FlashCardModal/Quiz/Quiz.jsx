039ff7bdad1d84e39f6947d27348a21167ff2b01
nhontx <kevintran351996@gmail.com>
2024-03-22 09:27:11 +0700

    update

--- src/components/FlashCardModal/Quiz/Quiz.jsx
+++ src/components/FlashCardModal/Quiz/Quiz.jsx
@@ -175,12 +175,6 @@ export default function PlayCountdown({
     }
   }, [isCorrect, index]);
 
-  useEffect(() => {
-    const [playSuccess] = useSound("/sound/success.mp3");
-    const [playWrong] = useSound("/sound/wrong.mp3");
-    playSuccess()
-    playWrong()
-  }, []);
 
   const handleAnswer = (img) => () => {
     // if (isCorrect != null) return
